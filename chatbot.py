d66ecc21be0521dae9297de9791fd2efa4f6e2c9
Maria-Teresa Carmier <143039024+mariyaaborisa@users.noreply.github.com>
2023-10-11 10:18:07 -0700

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,35 +1,44 @@
-# summary: 
-# produces a share-able url to the interface where user can record their speech and get a video response
-# uses gradio to build a user interface, has a mic input that takes in user's audio
-# speech-to-text the user audio, and use chatgpt to generate a response 
-# use aws polly to text-to-speech chatgpt's response, and generate an audio
-# give face image, audio/text response to d-id, which generates a speaking head video
-# play the video in gradio user interface
+# Summary:
+# This code generates a shareable URL for an interface where users can record their speech and receive a video response.
+# It uses Gradio to create a user interface with a microphone input for capturing user's audio.
+# The code performs speech-to-text on the user's audio and utilizes ChatGPT to generate a response.
+# It then uses AWS Polly to convert ChatGPT's response to text-to-speech and generate an audio.
+# The code provides a face image, audio/text response to D-ID, which generates a speaking head video.
+# Finally, it plays the video in the Gradio user interface.
 
-
-# local directory to store output video
+# Local directory to store the output video
 directory = ""
-# credentials for d_id, aws, openai, and cloudinary, please fill it in yourself
+
+# Credentials for D-ID, AWS, OpenAI, and Cloudinary; please fill in with your own credentials
 did_key = ""
-ACCESS_KEY = "" #open_file('accesskey.txt')
-SECRET_KEY = "" #open_file('secretkey.txt')
-polly = boto3.client('polly', region_name='us-east-1',aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
+ACCESS_KEY = ""  # Access key for AWS (e.g., 'AKIARMTEUYUB6VL2XMXF')
+SECRET_KEY = ""  # Secret key for AWS (e.g., 'Y2j8/V3Mllj5SoQtp4zEA8FzXLgpUapcG09te3ai')
+
+# Initialize AWS Polly client with specified region and credentials
+polly = boto3.client('polly', region_name='us-east-1', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
+
+# Create an AWS session and S3 client with the same credentials
 session = boto3.Session(aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
-s3 = session.client('s3',aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
-# ChatGPT的key，这里chatGPT主要用来将提问的语音转文本形式，然后通过文本形式的问题获取对应的AI文字回答
-openai.api_key = "" #open_file('openaiapikey.txt')
-# Cloudinary的Key，这里Cloudinary是做CDN，用来存文本、音频、视频这些东西，并能通过网络访问到
+s3 = session.client('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
+
+# Set the API key for ChatGPT, which is used to transcribe audio and generate AI text responses
+openai.api_key = ""  # Your OpenAI API key
+
+# Configure Cloudinary with your Cloudinary credentials, used for CDN services
 cloudinary.config(
-cloud_name = "",
-api_key = "",
-api_secret = ""
+    cloud_name="your_cloud_name",
+    api_key="your_api_key",
+    api_secret="your_api_secret"
 )
+
+# Define headers for API requests to D-ID
 headers = {
     "accept": "application/json",
     "content-type": "application/json",
     "authorization": "Basic " + did_key
 }
 
+# Import necessary Python modules and libraries
 import sys
 print(sys.executable)
 import urllib.request
@@ -49,113 +58,12 @@ import webbrowser
 import time
 import warnings
 
-gmh = "https://i.imgur.com/7Nnvv7d.png" # face image of george moses horton
-role_set = "You are George Moses Horton, an African American Poet. Please speak like George Moses Horton." # role set for chatgpt
+# Define the URL for a face image (George Moses Horton) and the role set for ChatGPT
+gmh = "https://i.imgur.com/7Nnvv7d.png"
+role_set = "You are George Moses Horton, an African American Poet. Please speak like George Moses Horton."
+
+# Ignore UserWarning messages
 warnings.filterwarnings("ignore", category=UserWarning)
-messages = [{"role": "system", "content": role_set}] # initialize messages
 
-def open_file(filepath):
-    with open(filepath, 'r', encoding='utf-8') as infile:
-        return infile.read()
-
-
-
-def upload_to_cloudinary(file_path):
-    response = cloudinary.uploader.upload(file_path, resource_type="video")
-    return response["secure_url"]
-
-	
-
-# get video url / download video based on talk id
-def check_video_status(video_id, headers):
-    
-    get_url = f"https://api.d-id.com/talks/{video_id}"
-    while True:
-        
-
-        headers = {
-            "accept": "application/json",
-            "authorization": "Basic " + did_key
-        }
-        get_response = requests.get(get_url, headers=headers)
-        video_status = get_response.json().get("status")
-
-        print(f"Video status: {get_response.json()}")
-        if video_status == "done":
-            return get_response.json().get("result_url")
-        elif video_status == "failed":
-            print("Video generation failed.")
-            return None
-        time.sleep(5)
-        print(get_url)
-
-def startfile(fn):
-    os.system('open %s' % fn)
-
-# process function for gradio, takes in audio to chatgpt and generate video from d_id
-def decipher(audio):
-    global messages
-    global audio_file
-    audio_file = open(audio, "rb")
-    # Using OpenAI's speech-to-text model
-    # speech to text the audio file
-    transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    messages.append({"role": "user", "content": transcript["text"]})
-    # lets chatgpt to chat complete based on user's input
-    response =  openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages
-    )
-
-    system_message = response["choices"][0]["message"]["content"]
-    messages.append({"role": "assistant", "content": system_message})
-
-    # use AWS Polly to tts chatgpt's response
-    voice_stream = polly.synthesize_speech(Text=system_message, VoiceId='Joey', OutputFormat='mp3')
-    with open('system_message.mp3', 'wb') as f:
-        f.write(voice_stream['AudioStream'].read())
-        f.close()
-
-    # Upload the audio file to cloudinary and get the public URL
-    audio_public_url = upload_to_cloudinary('system_message.mp3')
-
-    # D-ID API call
-    url = "https://api.d-id.com/talks"
-
-    payload = {
-    "script": {
-        "type": "text",
-        "provider": {
-            "type": "amazon",
-            "voice_id": "Joey"
-        },
-        "input": system_message,
-        "audio_url": audio_public_url
-    },
-    "source_url": "https://i.imgur.com/7Nnvv7d.png",
-    "driver_url": "bank://lively/driver-05",
-    "config": {
-        "stitch": "true"
-    }
-    }
-
-    # create video using d_id api call
-    response = requests.post(url, json=payload, headers=headers)
-    video_id = response.json().get("id")
-
-    # Make a GET request using the video_id
-    video_url = check_video_status(video_id, headers)
-    if video_url is not None:
-        webbrowser.open(video_url)
-    
-    chat_transcript = ""
-    for message in messages:
-        if message['role'] != "system":
-            chat_transcript += message['role'] + ": " + message['content'] + "\n\n"
-
-    urllib.request.urlretrieve(video_url, 'video_name.mp4') 
-    return chat_transcript, directory + "/video_name.mp4"
-
-# main function to launch the gradio interface, output is a shareable url.
-interface = gr.Interface(share=True, fn=decipher, inputs=gr.Audio(source="microphone", type="filepath"), outputs=["text", "playablevideo"])
-interface.launch()
+# Initialize a list of messages with a system message
+messages = [{"role": "system", "content": role_set}]
